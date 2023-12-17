6cc79299e02e3b7e80bcfef8b48de9fe0d3ae5da
jnguyen164 <jnguyen164@myseneca.ca>
2023-12-16 21:08:09 -0500

    revert text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <div>
             <div className="home--container">
-                <h1 className="home--title">titicakes</h1>
+                <h1 className="home--title">titicake</h1>
             </div>
             <Cart />
         </div>
